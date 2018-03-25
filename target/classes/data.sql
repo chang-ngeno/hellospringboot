47327faa534c1d896a544c6a38ac8e848cf4abf2
Daniel Chang'masa <danchangmasa@gmail.com>
2018-03-26 00:14:56 +0300

    Add files via upload

---
+++ target/classes/data.sql
@@ -0,0 +1,4 @@
+delete from member_user;
+insert into member_user(id, name) values(1,'Siva');
+insert into member_user(id, name) values(2,'Prasad');
+insert into member_user(id, name) values(3,'Reddy');
